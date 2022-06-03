518ef09e0ed2d2abc4d3dbfcc85d91516610c48e
vuvantinh1993 <46365041+vuvantinh1993@users.noreply.github.com>
2022-06-03 10:25:19 +0700

    sửa port

--- setup.sh
+++ setup.sh
@@ -91,8 +91,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=20000
-LAST_PORT=21000
+FIRST_PORT=3130
+LAST_PORT=4129
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
